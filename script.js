5853bbb25fb6525a5d37568c58f2315d4feede1c
rookie <yousufsiddiqui1996@gmail.com>
2018-09-14 11:38:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 document.getElementById('getUsers').addEventListener('click',getUsers);
 
+document.getElementById('getJson').addEventListener('click',getJson);
 
+document.getElementById('addPost').addEventListener('click',addPost);
 function getUsers(){
 
 fetch('users.json').
@@ -20,3 +22,45 @@ then( (response) => response.json() )
 
 
 }
+
+function getJson(){
+
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then( (response) => response.json() )
+    .then((data) => {
+        let output = `<h2>Posts<h2>`;
+        data.forEach((element) =>{
+    
+            output+=`<div>
+                    <h3>${element.title}</h3>
+                    <p>${element.body}</p>
+                    </div>`;
+            
+        });
+        document.getElementById('output').innerHTML = output;
+    })
+    
+    
+    
+    }
+    
+    
+function addPost(e){
+    e.preventDefault();
+
+    let title = document.getElementById('title');
+    let body = document.getElementById('body');
+
+    fetch('https://jsonplaceholder.typicode.com/posts',{
+        method:'POST',
+        headers:{
+            'Accept': 'application/json, text/plain, */*',
+            'content-type': 'application/json'
+        },
+        body:JSON.stringify({title:title,body:body})
+
+    }).then((res)=> res.json())
+    .then((data)=> console.log(data))    
+    
+    }
+    
